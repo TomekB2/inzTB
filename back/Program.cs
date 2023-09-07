ac7ed39e99057bdfe6e8aaf99395a14c039ea9e7
TomekB2 <126956626+TomekB2@users.noreply.github.com>
2023-09-07 08:36:02 +0200

    a

--- back/Program.cs
+++ back/Program.cs
@@ -7,8 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
 
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 
     app.UseSwagger();
