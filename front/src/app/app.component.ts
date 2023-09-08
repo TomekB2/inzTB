e7169dc103c57e6fea3a1dfa82d42009b55c7102
TomekB2 <126956626+TomekB2@users.noreply.github.com>
2023-09-08 08:42:54 +0200

    asd

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front';
+  constructor(
+    private http: HttpClient) { }
+  title = 'back';
+  configUrl = '/api/WeatherForecast';
+
+  tomek = this.getHeroes().subscribe();
+  getHeroes() {
+    return this.http.get<string>(this.configUrl);
+  }
 }
